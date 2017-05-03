ce408ff82e3536a5db28bbcdb82baea06dd0da47
Subhaneil Lahiri <subhylahiri@gmail.com>
2017-05-02 23:47:12 -0700

    typo

--- FigPageFix.m
+++ FigPageFix.m
@@ -2,7 +2,7 @@ function FigPageFix( figh )
 %FIGPAGEFIX(figh) Set page size for printing to pdf
 %   figh = figure object (handle)
 %   Sets figh.PaperPositionMode = 'auto'; 
-%   figh.Papersize = hFig.PaperPosition(3:4).
+%   figh.Papersize = figh.PaperPosition(3:4).
 %   Needs to be rerun if figh is resized.
 
 if ~exist('figh','var') || isempty(figh)
