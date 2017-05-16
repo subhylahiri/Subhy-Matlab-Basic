293861bc7f80fe3b59f5ca14a3f2cdf374ec43dd
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2017-05-16 14:28:29 -0700

    display progress of parfor

---
+++ DispCounterPar.m
@@ -0,0 +1,21 @@
+function DispCounterPar( i, imax )
+%msg=DISPCOUNTERPAR(i,imax,lab) Display progress of parfor loop
+%   i    = current value of loop counter, starting from 1
+%   imax = maximum value of loop counter
+%use DispCounter(0,imax,lab) to set up display
+%use DispCounter(imax+1,imax,lab) to delete final display
+
+if i==0
+    fprintf(['\n' repmat('.',1,imax) '\n\n']);
+elseif i>imax
+    fprintf(repmat('\b',1,imax+1));
+    fprintf(repmat('\b',1,imax+1));
+    fprintf('\b');
+else
+    fprintf('\b|\n');
+end
+
+
+
+end
+
