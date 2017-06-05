b2cfd61b897f94f33c7a78e95c31ceeb6c5a513b
Subhaneil Lahiri (work) <subhylahiri@gmail.com>
2017-06-05 16:28:01 -0700

    exclude vectors & scalars

--- InputTest/ismat.m
+++ InputTest/ismat.m
@@ -1,8 +1,8 @@
 function [ tf ] = ismat( m )
-%TF=ISMAT(M) is M a matrix? DEPRECATED: use ismatrix instead
+%TF=ISMAT(M) is M a matrix, and *not* a vector or scalar?
 %   TF=logical(0,1)
 
-tf = ismatrix(m);
+tf = ismatrix(m) && ~isvector(m);
 
 
 end
