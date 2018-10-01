ff7b3f1860337b5d37d0369c0a313cade3eb4779
Subhaneil Lahiri <subhylahiri@gmail.com>
2018-10-01 02:01:31 -0700

    clip range of numbers

---
+++ clip.m
@@ -0,0 +1,20 @@
+function [ clipped_vals ] = clip( varargin )
+%clipped_vals=CLIP(vals,lo,hi) Clip values to a range.
+%   vals: values to clip.
+%   lo:   lower bound of range, default = 0.
+%   hi:   upper bound of range, default = 1.
+
+persistent p
+if isempty(p)
+    p=inputParser;
+    p.addRequired('vals');
+    p.addOptional('lo', 0);
+    p.addOptional('hi', 1);
+end
+p.parse(varargin{:});
+r=p.Results;
+
+clipped_vals = min(max(r.vals, r.lo), r.hi);
+
+end
+
